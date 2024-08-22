e88db9d7bd450b802907e7bc2dd426660b0b827a
DannyGlade <darshannariya12@gmail.com>
2024-08-22 17:53:46 -0400

    chore: add react-native-reanimated plugin to babel configuration

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,9 @@ module.exports = {
   presets: [
     ['module:react-native-builder-bob/babel-preset', { modules: 'commonjs' }],
   ],
+  plugins: [
+    'react-native-reanimated/plugin',
+    // '@babel/plugin-proposal-optional-chaining',
+    // '@babel/plugin-proposal-nullish-coalescing-operator',
+  ],
 };
