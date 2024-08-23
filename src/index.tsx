f436a31a1da0157e746e0e8cbdea1479faca0788
DannyGlade <darshannariya12@gmail.com>
2024-08-23 15:13:43 -0400

    chore: add ring and rings-container components for animated rings

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import WavyRings from './WavyRings';
+import Ring from './WavyRings/Ring';
+import RingsContainer from './WavyRings/RingsContainer';
 
 export type { WavyRingsProps } from './WavyRings';
 export type { RingProps } from './WavyRings/Ring';
+export type { RingsContainerProps } from './WavyRings/RingsContainer';
 
 export default WavyRings;
+export { Ring, RingsContainer };
