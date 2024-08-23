fe0e70852acdafc0c7a3143282b7f56ee346d03e
DannyGlade <darshannariya12@gmail.com>
2024-08-23 15:13:11 -0400

    chore: update wavy-rings component to include rings-container and additional styling options

--- src/WavyRings/index.tsx
+++ src/WavyRings/index.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, type StyleProp, type ViewStyle } from 'react-native';
 import Ring from './Ring';
 import type { PropsWithChildren } from 'react';
+import RingsContainer, { type RingsContainerProps } from './RingsContainer';
 
 export type WavyRingsProps = PropsWithChildren<{
   rings: number;
@@ -8,20 +9,46 @@ export type WavyRingsProps = PropsWithChildren<{
   height?: number;
   delay: number;
   isRunning: boolean;
-}>;
+  ringsStyle?: StyleProp<ViewStyle>;
+  childrenContainerStyle?: StyleProp<ViewStyle>;
+}> &
+  RingsContainerProps;
 
-const WavyRings = ({
+/**
+ * WavyRings component - default rings wrapper
+ *
+ * @param {Number} rings - Number of rings
+ * @param {Number} width - Width of the WavyRings
+ * @param {Number} height - `Optional` Height of the WavyRings
+ * @param {Number} delay - Delay between each ring
+ * @param {Boolean} isRunning - Animation state
+ * @param {Object} containerStyle - `Optional` Custom container styles
+ * @param {Object} boxStyle - `Optional` Custom box styles
+ * @param {Object} ringsStyle - `Optional` Custom ring styles
+ * @param {Object} childrenContainerStyle - `Optional` Custom children container styles
+ * @param {ReactNode} children - `Optional` Children components
+ *
+ */
+export default function WavyRings({
   children,
   rings,
   width,
   height,
   delay,
   isRunning,
-}: WavyRingsProps) => {
+  containerStyle,
+  boxStyle,
+  ringsStyle,
+  childrenContainerStyle,
+}: WavyRingsProps) {
   return (
     <>
-      <View style={styles.container}>
-        <View style={[styles.box, { width: width, height: height ?? width }]} />
+      <RingsContainer
+        width={width}
+        height={height}
+        containerStyle={containerStyle}
+        boxStyle={boxStyle}
+      >
         {Array.from({ length: rings }).map((_, index) => (
           <Ring
             key={index}
@@ -29,19 +56,19 @@ const WavyRings = ({
             width={width}
             height={height}
             isRunning={isRunning}
+            style={[styles.ring, ringsStyle]}
           />
         ))}
-        <View style={styles.childrenBox}>{children}</View>
-      </View>
+        <View style={[styles.childrenBox, childrenContainerStyle]}>
+          {children}
+        </View>
+      </RingsContainer>
     </>
   );
-};
-
-export default WavyRings;
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -52,6 +79,9 @@ const styles = StyleSheet.create({
     borderColor: 'rgba(0, 0, 0, 0.1)',
     zIndex: 1,
     position: 'absolute',
+  },
+  ring: {
+    borderColor: '#4486f8',
   },
   childrenBox: {
     position: 'absolute',
